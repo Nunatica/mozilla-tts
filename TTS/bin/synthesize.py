580f85d6a7506e7e82ce807d1975e9ae38bbee35
Nunatica <alternate.sec@gmail.com>
2021-05-04 04:01:27 +0900

    Fixed output path.

--- TTS/bin/synthesize.py
+++ TTS/bin/synthesize.py
@@ -206,12 +206,8 @@ def main():
     wav = synthesizer.tts(args.text)
 
     # save the results
-    file_name = args.text.replace(" ", "_")[0:20]
-    file_name = file_name.translate(
-        str.maketrans('', '', string.punctuation.replace('_', ''))) + '.wav'
-    out_path = os.path.join(args.out_path, file_name)
-    print(" > Saving output to {}".format(out_path))
-    synthesizer.save_wav(wav, out_path)
+    print(" > Saving output to {}".format(args.out_path))
+    synthesizer.save_wav(wav, args.out_path)
 
 
 if __name__ == "__main__":
